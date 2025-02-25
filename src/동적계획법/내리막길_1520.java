9aa85c196ea401773cfc6d7848b8606deca74d95
mintaek22 <biba99@naver.com>
2025-02-25 10:45:10 +0900

    dp:1520

---
+++ src/동적계획법/내리막길_1520.java
@@ -0,0 +1,60 @@
+package 동적계획법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 내리막길_1520 {
+
+    static int N, M;
+    static int[][] map, dp;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        dp = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(dfs(0, 0));
+    }
+
+    static int dfs(int x, int y) {
+        if (x == N - 1 && y == M - 1) {
+            return 1;
+        }
+
+        if (dp[x][y] != -1) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+
+            if (newX >= 0 && newX < N && newY >= 0 && newY < M) {
+                if (map[newX][newY] < map[x][y]) {
+                    dp[x][y] += dfs(newX, newY);
+                }
+            }
+        }
+
+        return dp[x][y];
+    }
+}
