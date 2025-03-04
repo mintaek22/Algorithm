5df72a4865f79cc67316df680ca7e94651cca1ea
mintaek22 <biba99@naver.com>
2025-03-04 23:52:52 +0900

    back tracking:1174

--- src/백트래킹/줄어드는수_1174.java
+++ src/백트래킹/줄어드는수_1174.java
@@ -3,57 +3,41 @@ package 백트래킹;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class 줄어드는수_1174 {
 
     static int N;
-    static ArrayList<String> arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        int len = 1;
-        while (N > 0) {
-            int start = N;
-            arr = new ArrayList<>();
-            arr.add("10");
-            dfs(len,0);
-            len++;
-            int end = N;
-            if(start==end) {
-                System.out.println(-1);
+        Queue<String> q= new LinkedList<>();
+        q.add("");
+        int count = -1;
+        while(!q.isEmpty()){
+            String s = q.poll();
+            count++;
+            if(count == N){
+                System.out.println(s);
                 return;
             }
-        }
-    }
-
-    static public void dfs(int len,int depth){
-        if(N == 0) return;
-        if(len == depth){
-            N--;
-            if(N==0){
-                for(String s:arr){
-                    if(s.equals("10")) continue;
-                    System.out.print(s);
+            for (int i = 0; i <= 9 ; i++) {
+                if(s.isEmpty()){
+                    q.add(String.valueOf(i));
+                }
+                else{
+                    if(Integer.parseInt(String.valueOf(s.charAt(s.length()-1)))>i){
+                        q.add(s+i);
+                    }
+                    else break;
                 }
             }
         }
-        if(depth == 0 && len > 1){
-            for (int i = 1; i < 10; i++) {
-                arr.add(String.valueOf(i));
-                dfs(len,depth+1);
-                arr.remove(arr.size()-1);
-            }
-        }
-        else{
-            for (int i = 0; i < Integer.parseInt(arr.get(arr.size()-1)); i++) {
-                arr.add(String.valueOf(i));
-                dfs(len,depth+1);
-                arr.remove(arr.size()-1);
-            }
-        }
 
+        System.out.println(-1);
     }
+
 }
