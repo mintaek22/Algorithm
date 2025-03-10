7a7d6f5a4524e72325246aa0dd27f9470302946b
mintaek22 <biba99@naver.com>
2025-03-10 14:49:03 +0900

    priority queue: 7662

---
+++ src/우선순위큐/이중우선순위큐_7662.java
@@ -0,0 +1,70 @@
+package 우선순위큐;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class 이중우선순위큐_7662 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            Map<Integer, Integer> map = new HashMap<>();
+            PriorityQueue<Integer> minQue = new PriorityQueue<>();
+            PriorityQueue<Integer> maxQue = new PriorityQueue<>(Collections.reverseOrder());
+
+            for (int i = 0; i < k; i++) {
+                String[] input = br.readLine().split(" ");
+                char ch = input[0].charAt(0);
+                int n = Integer.parseInt(input[1]);
+
+                if (ch == 'I') {
+                    map.put(n, map.getOrDefault(n, 0) + 1);
+
+                    minQue.add(n);
+                    maxQue.add(n);
+                } else {
+                    if (map.isEmpty())
+                        continue;
+
+                    PriorityQueue<Integer> que = n == 1 ? maxQue : minQue;
+                    removeMap(que, map);
+                }
+            }
+
+            if (map.isEmpty())
+                System.out.println("EMPTY");
+            else {
+                int n = removeMap(maxQue, map);
+                System.out.println(n + " " + (!map.isEmpty() ? removeMap(minQue, map) : n));
+            }
+
+        }
+
+    }
+
+
+    static int removeMap(PriorityQueue<Integer> que, Map<Integer, Integer> map) {
+        int num;
+        while (true) {
+            num = que.poll();
+            int cnt = map.getOrDefault(num, 0);
+
+            if (cnt == 0)
+                continue;
+
+            if (cnt == 1)
+                map.remove(num);
+            else
+                map.put(num, cnt - 1);
+
+            break;
+        }
+
+        return num;
+    }
+}
